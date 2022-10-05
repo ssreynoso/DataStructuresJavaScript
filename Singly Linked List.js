be1d9c5485a1544464659db61d3b731a4c2299d6
ssreynoso <s.reynosodunjo@gmail.com>
2022-10-05 18:44:12 -0300

    Trato de terminar pila y agrego propiedades privadas con #

--- Singly Linked List.js
+++ Singly Linked List.js
@@ -1,45 +1,46 @@
 class Node {
-  constructor(value){
-    this.value = value
-    this.next  = null
-  }
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
 class SinglyLinkedList {
-  constructor() {
-    this.head = null
-    this.tail = null
-    this.length = 0
-  }
-  append(value){
-    const nodo = new Node(value)
-    if (this.length === 0) {
-      this.head = nodo
-    } else {
-      this.tail.next = nodo
+    #head   = null;
+    #tail   = null;
+    #length = 0;
+
+    constructor() {}
+    
+    append(value) {
+        const nodo = new Node(value);
+        if (this.#length === 0) {
+            this.#head = nodo;
+        } else {
+            this.#tail.next = nodo;
+        }
+        this.#tail = nodo;
+        this.#length++;
+        return nodo;
     }
-	  this.tail = nodo
-    this.length++
-    return nodo
-  }
-  
-  prepend(value) {
-    const nodo = new Node(value)
-    if (this.length === 0) {
-      this.tail = nodo
-    } else {
-      nodo.next = this.head
+
+    prepend(value) {
+        const nodo = new Node(value);
+        if (this.#length === 0) {
+            this.#tail = nodo;
+        } else {
+            nodo.next = this.#head;
+        }
+        this.#head = nodo;
+        this.#length++;
+        return nodo;
     }
-    this.head = nodo
-    this.length++
-    return nodo
-  }
-  
-  // implementar insert
-  // averiguar cómo hacer métodos y propiedades privadas
-  // implementar remove
+
+    // implementar insert
+    // averiguar cómo hacer métodos y propiedades privadas
+    // implementar remove
 }
 
-const lista = new SinglyLinkedList()
-lista.append(2)
-lista.prepend(3)
+const lista = new SinglyLinkedList();
+lista.append(2);
+lista.prepend(3);
--- Stack.js
+++ Stack.js
@@ -6,29 +6,50 @@ class Node {
 }
 
 class Stack {
-    constructor() {
-        this.top = null;
-        this.bottom = null;
-        this.length = 0;
-    }
+
+    #top    = null;
+    #bottom = null;
+    #length = 0;
+
+
+    constructor() {}
 
     peek() {
-      	if (this.length === 0) return null;
-        return this.top.value;
+      	if (this.#length === 0) return null;
+        return this.#top.value;
     }
 
     pop(value) {
-      	if (this.length === 0) return null;
+      	if (this.#length === 0) return null;
+        if (this.#length === 1) this.#bottom = null;
         // Tengo que sacar y devolver al último
-        const nodo = this.top
-        this.top = nodo.next
+        const nodo = this.#top
+        this.#top   = nodo.next
+        this.#length--
       	return nodo.value
     }
 
     push(value) {
         const newNode = new Node(value);
-      	newNode.value = value
-      	newNode.next = this.top
-      	this.top = newNode
+      	newNode.next  = this.#top
+      	this.#top     = newNode
+        if (this.#length === 0){
+            this.#bottom = newNode 
+        }
+        this.#length++
+        return newNode.value
     }
 }
+
+const pila = new Stack()
+console.log(pila.peek())
+console.log(pila.pop())
+console.log(pila.push(10))
+console.log(pila.push(20))
+console.log(pila.push(30))
+console.log(pila)
+console.log(pila.pop())
+console.log(pila.pop())
+console.log(pila.peek())
+console.log(pila.pop())
+console.log(pila)
